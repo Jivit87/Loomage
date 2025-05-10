c89ae102710c25d26f48ca13ea6efdfc674d803b
Jivit87 <jivitrana87@gmail.com>
2025-05-10 22:08:00 +0530

    Added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { AppContext } from "./context/AppContext";
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const {showLogin} = useContext(AppContext);
 
-export default App
+  return (
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <ToastContainer position="bottom-right"/>
+      <Navbar/>
+      {showLogin && <Login/>}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonals from '../components/Testimonals'
+import GenerateBtn from '../components/GenerateBtn'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Steps/>
+        <Description/>
+        <Testimonals/>
+        <GenerateBtn/>
+       
+    </div>
+  )
+}
+
+export default Home
